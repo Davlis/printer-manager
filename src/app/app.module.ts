dcdf736e9db040f1cd853822f3c1e638d662cabe
davlis <ddewid@gmail.com>
2018-05-12 00:54:37 +0200

    added printer-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PrinterListComponent } from './printer-list/printer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrinterListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/printer-list/printer-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-printer-list',
+  templateUrl: './printer-list.component.html',
+  styleUrls: ['./printer-list.component.css']
+})
+export class PrinterListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
