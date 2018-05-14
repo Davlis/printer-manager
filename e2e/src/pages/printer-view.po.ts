f78b2e1796968610d530af5161a01d756eb0de65
davlis <ddewid@gmail.com>
2018-05-14 03:52:51 +0200

    refactoring to e2e tests

--- e2e/src/pages/printer-view.po.ts
+++ e2e/src/pages/printer-view.po.ts
@@ -6,23 +6,15 @@ export class PrinterViewPage {
     }
 
     getTypeInput() {
-        return element.all(by.css('p-dropdown')).get(0).click();
-    }
-
-    clickNeedleOpt() {
-        return element(by.cssContainingText('span', 'Needle')).click();
+        return element.all(by.css('p-dropdown')).get(0);
     }
 
     getStatusInput() {
-        return element.all(by.css('p-dropdown')).get(1).click();
+        return element.all(by.css('p-dropdown')).get(1);
     }
 
-    clickOnlineOpt() {
-        return element(by.cssContainingText('span', 'Online')).click();
-    }
-
-    getNetworkInput() {
-        return element.all(by.css('input')).get(3);
+    getNetworkAddressInput() {
+        return element.all(by.css('input')).get(5);
     }
 
     getDescriptionInput() {
@@ -30,11 +22,47 @@ export class PrinterViewPage {
     }
 
     getColorInput() {
-        return element.all(by.css('input')).get(5);
+        return element.all(by.css('input')).get(6);
+    }
+
+    setName(name) {
+        const el = this.getNameInput();
+        el.sendKeys(name);
+    }
+
+    setType(type) {
+        const el = this.getTypeInput();
+        el.click();
+        element(by.cssContainingText('span', type)).click();
+    }
+
+    setStatus(status) {
+        const el = this.getStatusInput();
+        el.click();
+        element(by.cssContainingText('span', status)).click();
+    }
+
+    setNetworkAddress(address) {
+        const el = this.getNetworkAddressInput();
+        el.sendKeys(address);
+    }
+
+    setDescription(description) {
+        const el = this.getDescriptionInput();
+        el.sendKeys(description);
+    }
+
+    setColor(color) {
+        const el = this.getColorInput();
+        el.sendKeys(color);
+    }
+
+    getForm() {
+        return element(by.css('form'));
     }
 
     submitForm() {
-        const el = element(by.css('form'));
+        const el = this.getForm();
         el.submit();
     }
 }
--- e2e/src/specs/printer.e2e-spec.ts
+++ e2e/src/specs/printer.e2e-spec.ts
@@ -13,26 +13,23 @@ describe('Printer', () => {
     it('should add new printer', () => {
         const PRINTER = {
             name: 'Prrinter',
+            type: 'Needle',
+            status: 'Online',
             ipAddress: '192.168.0.13',
             description: 'My printer',
-            color: 'Red'
+            color: 'Red',
         };
 
         listPage.navigateTo();
         const btn = listPage.getAddButton();
         btn.click();
-        const name = viewPage.getNameInput();
-        name.sendKeys(PRINTER.name);
-        viewPage.getTypeInput();
-        viewPage.clickNeedleOpt();
-        viewPage.getStatusInput();
-        viewPage.clickOnlineOpt();
-        const network = viewPage.getNetworkInput();
-        network.sendKeys(PRINTER.ipAddress);
-        const description = viewPage.getDescriptionInput();
-        description.sendKeys(PRINTER.description);
-        const color = viewPage.getColorInput();
-        color.sendKeys(PRINTER.color);
+
+        viewPage.setName(PRINTER.name);
+        viewPage.setType(PRINTER.type);
+        viewPage.setStatus(PRINTER.status);
+        viewPage.setNetworkAddress(PRINTER.ipAddress);
+        viewPage.setDescription(PRINTER.description);
+        viewPage.setColor(PRINTER.color);
         viewPage.submitForm();
         expect(listPage.getFirstElementName()).toEqual(PRINTER.name);
     });
